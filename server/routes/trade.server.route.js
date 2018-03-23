43f1a7f23fdbd6a4c676e3ed5da61f0552493941
zilong <zilong@zilongdeiMac.local>
2018-03-23 19:13:10 +0800

    接口调试

--- server/routes/trade.server.route.js
+++ server/routes/trade.server.route.js
@@ -5,41 +5,18 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-
-    //不带参数访问显示列表页
-    /*res.render(TEMP_TRADE, {
-        templateMain : 'trade/list.html',
-        market : 'null'
-    });*/
-    //获取默认市场
-    var reqProtocol = '';
-    if(ENV == 'w' || ENV == 't'){
-        reqProtocol = 'http:';
-    }else{
-        reqProtocol = 'https:';
-    }
-    res.redirect(reqProtocol + '' + DOMAIN_MAIN + '/trade');
-});
-
-router.get('/:market', function(req, res, next) {
+router.get(['/', '/:market'], function(req, res, next) {
     var market = req.params.market;
 
-    //通过配置文件排查市场是否存在并可用
-    // var isMarket = false;
-    // for(var key in MARKET){
-    //     if(market == key && !MARKET[key].disable){
-    //         isMarket = true;
-    //     }
-    // }
-    // if(!isMarket){
-    //     res.redirect('/trade/etc_btc');
-    // }
-    //页面渲染
-    res.render(TEMP_TRADE, {
-        templateMain : 'tradePro/index.html',
-        market : market
-    });
+    if(!market){
+        res.redirect('/trade/etc_btc');
+    }else{
+        //页面渲染
+        res.render(TEMP_TRADE, {
+            templateMain : 'tradePro/index.html',
+            market : market
+        });
+    }
 });
 
 module.exports = router;
